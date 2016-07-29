70a99323f3691dac8cdaf7c678a8da096c74c0bc
pearpages <pere@soms.cat>
2016-07-29 15:53:56 +0200

    description component

--- src/app/components/description/description.component.ts
+++ src/app/components/description/description.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   moduleId: module.id,
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['description.component.css']
 })
 export class DescriptionComponent implements OnInit {
+
+  @Input() account: any;
 
   constructor() {}
 
